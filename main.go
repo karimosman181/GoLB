6b7dfdde720b5e6b7450dc8b85d83bd8ded1e1eb
Karim Osman <kareemosman199@gmail.com>
2023-07-27 21:58:32 +0300

    add loadbalancer struct

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
 )
 
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, r *http.Request)
+}
 type simpleServer struct {
 	addr  string
 	proxy *httputil.ReverseProxy
+}
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server
 }
 
 /**
@@ -27,6 +39,18 @@ func newSimpleServer(addr string) *simpleServer {
 		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
 	}
 
+}
+
+/**
+ *
+ * func to create and initialize a new Load Balancer
+ **/
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
+	}
 }
 
 /**
